fe69ff49db5926121abe2c2938020fe5c8a84b01
Esteban Gonzalez <estebangz1896@gmail.com>
2021-05-04 13:20:24 -0600

    Return Workshop

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 const input = document.querySelector('.numberInput');
-const para = document.querySelector('p');
+const paragraph = document.querySelector('p');
+
+function squared(number) {
+    return number * number;
+}
+
+function cubed(number) {
+    return number*number*number;
+}
+
+function factorial(number) {
+    if (number < 0) return undefined;
+    if (number == 0) return 1;
+    let x = number -1;
+    while(x > 1) {
+        number *= x;
+        x--;
+    }
+    return number;
+}
+
+input.onchange = function() {
+    const number = input.value;
+    if (isNaN(number)) {
+        paragraph.textContent = "Please enter a number!"
+    } else {
+        paragraph.textContent = `${number} squared is ${squared(number)}.
+        ${number} cubed is ${cubed(number)}.
+        ${number} factorial is ${factorial(number)}.`;
+    }
+}
